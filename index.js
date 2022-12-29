07aedba1e08ea89bc5382b8c11b1f781a1dce32f
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-29 09:56:41 -0500

    Implement functions to fetch IP and geolocation based on that IP

---
+++ index.js
@@ -0,0 +1,18 @@
+const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+
+/* fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+}); */
+
+/* fetchCoordsByIP('66.207.204.60', (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
+}); */
---
+++ iss.js
@@ -0,0 +1,45 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+  
+    const data = JSON.parse(body);
+
+    if (!data.success) {
+      const message = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    
+    const { latitude, longitude } = data;
+    callback(null, {latitude, longitude});
+    return;
+  });
+  
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
