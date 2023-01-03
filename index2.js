f687ab4d02679e8060999f8875763748bcdfeda8
Ozzy Tasci <oguzcantasci@gmail.com>
2023-01-03 11:46:52 -0500

    Implement promise-based error handling

--- index2.js
+++ index2.js
@@ -16,4 +16,7 @@ const printPassTimes = function(passTimes) {
 nextISSTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
   });
